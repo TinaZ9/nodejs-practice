555622fcaeded5101d573a49e2cf88686d332213
Oreinz M. Kasilag <oreinzk@yahoo.com>
2022-11-27 13:27:31 +0800

    added math.js

--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-console.log("hello")
+
+"use strict"
+const {add, subtract, multiply, divide} = require('./math')
+
+
+
+console.log(add(2, 3));
+console.log(subtract(2, 3));
+console.log(multiply(2, 3));
+console.log(divide(2, 3));
+
+
+// console.log("hello")
+
+// //How NodeJS differs from Vanilla JS
+
+
+
+// // 5) CommonJS modules instead of ES6
+
+
+// const os = require('os');
+// const path = require('path');
+
+// console.log(os.type());
+// console.log(os.version());
+// console.log(os.homedir());
+
+
+// console.log(__dirname);
+// console.log(__filename);
+
+// console.log(path.dirname(__filename));
+// console.log(path.basename(__filename));
+// console.log(path.extname(__filename));
+
+// console.log(path.parse(__filename));
