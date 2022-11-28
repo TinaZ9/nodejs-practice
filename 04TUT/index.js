5f884c7e46e18b512daf10e6ce138190ce334217
Oreinz M. Kasilag <oreinzk@yahoo.com>
2022-11-28 19:38:50 +0800

    Add New Tutorial

---
+++ 04TUT/index.js
@@ -0,0 +1,20 @@
+const logEvents = require('./logEvents');
+
+const EventEmitter = require('events');
+
+
+class MyEmitter extends EventEmitter {};
+
+//Initialize object
+const myEmitter = new MyEmitter();
+
+//addlistner for the log event
+myEmitter.on('log', msg => logEvents(msg));
+
+
+setTimeout(() => {
+    
+    //Emit event
+    myEmitter.emit('log', "Log event emitted");
+
+}, 2000)
